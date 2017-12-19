9936308b403b303cd3cd04bcf9ba5bcd51d8e26b
Tad Hardesty <tad@platymuus.com>
2017-12-18 17:49:25 -0800

    Add skeleton of language server

---
+++ src/langserver/main.rs
@@ -0,0 +1,14 @@
+//! DreamMaker language server.
+//!
+//! Based on:
+//!
+//! * https://langserver.org/
+//! * https://github.com/Microsoft/language-server-protocol/blob/master/versions/protocol-2-x.md
+//! * https://github.com/rust-lang-nursery/rls
+
+extern crate languageserver_types;
+extern crate jsonrpc_core;
+extern crate dreammaker as dm;
+
+fn main() {
+}
