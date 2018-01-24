174ca490a0891a43f890fac4ccb6553fa749e1cf
Tad Hardesty <tad@platymuus.com>
2018-01-23 23:41:09 -0800

    Make the editor its own crate and the root just a workspace

--- src/editor/build.rs
+++ src/editor/build.rs
@@ -6,15 +6,15 @@ fn main() {
     if cfg!(windows) {
         let out_dir = env::var("OUT_DIR").ok().expect("can't find out_dir");
 
-        Command::new("windres").args(&["src/editor/editor.rc",  "-o"])
+        Command::new("windres").args(&["editor.rc",  "-o"])
                         .arg(&format!("{}/editor.rc.o", out_dir))
                         .status().unwrap();
         Command::new("ar").args(&["crus", "libeditor_rc.a", "editor.rc.o"])
                         .current_dir(Path::new(&out_dir))
                         .status().unwrap();
 
-        println!("cargo:rerun-if-changed=src/editor/editor.rc");
-        println!("cargo:rerun-if-changed=src/editor/gasmask.ico");
+        println!("cargo:rerun-if-changed=editor.rc");
+        println!("cargo:rerun-if-changed=gasmask.ico");
         println!("cargo:rustc-link-search=native={}", out_dir);
         println!("cargo:rustc-link-lib=static=editor_rc");
     }
