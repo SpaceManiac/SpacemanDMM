bf970f5c5f7ba80ca57c475d35b8a5b29dcbe15d
Tad Hardesty <tad@platymuus.com>
2018-02-09 22:49:32 -0800

    Port transit tube corners to a render pass

--- src/tools/minimap.rs
+++ src/tools/minimap.rs
@@ -106,8 +106,6 @@ pub fn generate(
                         copy.set_var("icon_state", keyboard.clone());
                         overlays.push(copy);
                     }
-                } else if subtype(p, "/obj/structure/transit_tube/") {
-                    generate_tube_overlays(&mut overlays, ctx, &atom);
                 } else if subtype(p, "/obj/machinery/door/airlock/") {
                     let mut copy = atom.clone();
                     if atom.get_var("glass", objtree).to_bool() {
@@ -360,7 +358,7 @@ impl<'a> Atom<'a> {
         None
     }
 
-    fn copy_var(&mut self, key: &str, from: &Atom, objtree: &'a ObjectTree) {
+    pub fn copy_var(&mut self, key: &str, from: &Atom, objtree: &'a ObjectTree) {
         if let Some(var) = from.get_var_notnull(key, objtree) {
             self.set_var(key, var.clone());
         }
@@ -711,111 +709,3 @@ fn right_45(dir: i32) -> i32 {
     }
 }
 
-// ----------------------------------------------------------------------------
-// Transit tube smoothing
-
-fn generate_tube_overlays<'a>(output: &mut Vec<Atom<'a>>, ctx: Context<'a>, source: &Atom<'a>) {
-    use dmi::*;
-
-    let dir = source.get_var("dir", ctx.objtree).to_int().unwrap_or(::dmi::SOUTH);
-
-    let mut fulfill = |items: &[i32]| {
-        for &dir in items {
-            if dir == NORTHEAST || dir == NORTHWEST || dir == SOUTHEAST || dir == SOUTHWEST {
-                if dir & NORTH != 0 {
-                    create_tube_overlay(output, ctx, source, dir ^ 3, NORTH);
-                    if dir & EAST != 0 {
-                        create_tube_overlay(output, ctx, source, dir ^ 12, EAST);
-                    } else {
-                        create_tube_overlay(output, ctx, source, dir ^ 12, WEST);
-                    }
-                }
-            } else {
-                create_tube_overlay(output, ctx, source, dir, 0);
-            }
-        }
-    };
-
-    let p = &source.type_.path;
-    if subtype(p, "/obj/structure/transit_tube/station/reverse/") {
-        fulfill(&match dir {
-            NORTH => [EAST],
-            SOUTH => [WEST],
-            EAST => [SOUTH],
-            WEST => [NORTH],
-            _ => return,
-        })
-    } else if subtype(p, "/obj/structure/transit_tube/station/") {
-        fulfill(&match dir {
-            NORTH | SOUTH => [EAST, WEST],
-            EAST | WEST => [NORTH, SOUTH],
-            _ => return,
-        })
-    } else if subtype(p, "/obj/structure/transit_tube/junction/flipped/") {
-        fulfill(&match dir {
-            NORTH => [NORTH, SOUTHWEST, SOUTHEAST],
-            SOUTH => [SOUTH, NORTHEAST, NORTHWEST],
-            EAST => [EAST, NORTHWEST, SOUTHWEST],
-            WEST => [WEST, SOUTHEAST, NORTHEAST],
-            _ => return,
-        })
-    } else if subtype(p, "/obj/structure/transit_tube/junction/") {
-        fulfill(&match dir {
-            NORTH => [NORTH, SOUTHEAST, SOUTHWEST],
-            SOUTH => [SOUTH, NORTHWEST, NORTHEAST],
-            EAST => [EAST, SOUTHWEST, NORTHWEST],
-            WEST => [WEST, NORTHEAST, SOUTHEAST],
-            _ => return,
-        })
-    } else if subtype(p, "/obj/structure/transit_tube/curved/flipped/") {
-        fulfill(&match dir {
-            NORTH => [NORTH, SOUTHEAST],
-            SOUTH => [SOUTH, NORTHWEST],
-            EAST => [EAST, SOUTHWEST],
-            WEST => [NORTHEAST, WEST],
-            _ => return,
-        })
-    } else if subtype(p, "/obj/structure/transit_tube/curved/") {
-        fulfill(&match dir {
-            NORTH => [NORTH, SOUTHWEST],
-            SOUTH => [SOUTH, NORTHEAST],
-            EAST => [EAST, NORTHWEST],
-            WEST => [SOUTHEAST, WEST],
-            _ => return,
-        })
-    } else if subtype(p, "/obj/structure/transit_tube/diagonal/") {
-        fulfill(&match dir {
-            NORTH | SOUTH => [NORTHEAST, SOUTHWEST],
-            EAST | WEST => [NORTHWEST, SOUTHEAST],
-            _ => return,
-        })
-    } else {
-        fulfill(&match dir {
-            NORTH | SOUTH => [NORTH, SOUTH],
-            EAST | WEST => [EAST, WEST],
-            _ => return,
-        })
-    }
-}
-
-fn create_tube_overlay<'a>(output: &mut Vec<Atom<'a>>, ctx: Context<'a>, source: &Atom<'a>, dir: i32, shift: i32) {
-    use dmi::*;
-
-    let mut copy = Atom::from_type(ctx.objtree, "/atom", source.loc).unwrap();
-    copy.set_var("dir", Constant::Int(dir));
-    copy.copy_var("layer", source, ctx.objtree);
-    copy.copy_var("icon", source, ctx.objtree);
-    if shift != 0 {
-        copy.set_var("icon_state", Constant::string("decorative_diag"));
-        match shift {
-            NORTH => copy.set_var("pixel_y", Constant::Int(32)),
-            SOUTH => copy.set_var("pixel_y", Constant::Int(-32)),
-            EAST => copy.set_var("pixel_x", Constant::Int(32)),
-            WEST => copy.set_var("pixel_x", Constant::Int(-32)),
-            _ => {}
-        }
-    } else {
-        copy.set_var("icon_state", Constant::string("decorative"));
-    }
-    output.push(copy);
-}
--- src/tools/render_passes/mod.rs
+++ src/tools/render_passes/mod.rs
@@ -77,6 +77,7 @@ pub const RENDER_PASSES: &[RenderPassInfo] = &[
     pass!(Random, "random", "Replace random spawners with one of their possibilities.", true),
     pass!(Pretty, "pretty", "Add the minor cosmetic overlays for various objects.", true),
     pass!(Spawners, "spawners", "Replace object spawners with their spawned objects.", true),
+    pass!(TransitTube, "transit-tube", "Add overlays to connect transit tubes together.", true),
 ];
 
 pub fn configure(include: &str, exclude: &str) -> Vec<Box<RenderPass>> {
@@ -274,4 +275,121 @@ impl RenderPass for Pretty {
             }
         }
     }
+}
+
+#[derive(Default)]
+pub struct TransitTube;
+impl RenderPass for TransitTube {
+    fn overlays<'a>(&self,
+        atom: &mut Atom<'a>,
+        objtree: &'a ObjectTree,
+        _: &mut Vec<Atom<'a>>,
+        overlays: &mut Vec<Atom<'a>>,
+    ) {
+        use dmi::*;
+
+        if !atom.istype("/obj/structure/transit_tube/") {
+            return
+        }
+
+        let dir = atom.get_var("dir", objtree).to_int().unwrap_or(::dmi::SOUTH);
+        let mut fulfill = |items: &[i32]| {
+            for &dir in items {
+                if dir == NORTHEAST || dir == NORTHWEST || dir == SOUTHEAST || dir == SOUTHWEST {
+                    if dir & NORTH != 0 {
+                        create_tube_overlay(overlays, objtree, atom, dir ^ 3, NORTH);
+                        if dir & EAST != 0 {
+                            create_tube_overlay(overlays, objtree, atom, dir ^ 12, EAST);
+                        } else {
+                            create_tube_overlay(overlays, objtree, atom, dir ^ 12, WEST);
+                        }
+                    }
+                } else {
+                    create_tube_overlay(overlays, objtree, atom, dir, 0);
+                }
+            }
+        };
+
+        if atom.istype("/obj/structure/transit_tube/station/reverse/") {
+            fulfill(&match dir {
+                NORTH => [EAST],
+                SOUTH => [WEST],
+                EAST => [SOUTH],
+                WEST => [NORTH],
+                _ => return,
+            })
+        } else if atom.istype("/obj/structure/transit_tube/station/") {
+            fulfill(&match dir {
+                NORTH | SOUTH => [EAST, WEST],
+                EAST | WEST => [NORTH, SOUTH],
+                _ => return,
+            })
+        } else if atom.istype("/obj/structure/transit_tube/junction/flipped/") {
+            fulfill(&match dir {
+                NORTH => [NORTH, SOUTHWEST, SOUTHEAST],
+                SOUTH => [SOUTH, NORTHEAST, NORTHWEST],
+                EAST => [EAST, NORTHWEST, SOUTHWEST],
+                WEST => [WEST, SOUTHEAST, NORTHEAST],
+                _ => return,
+            })
+        } else if atom.istype("/obj/structure/transit_tube/junction/") {
+            fulfill(&match dir {
+                NORTH => [NORTH, SOUTHEAST, SOUTHWEST],
+                SOUTH => [SOUTH, NORTHWEST, NORTHEAST],
+                EAST => [EAST, SOUTHWEST, NORTHWEST],
+                WEST => [WEST, NORTHEAST, SOUTHEAST],
+                _ => return,
+            })
+        } else if atom.istype("/obj/structure/transit_tube/curved/flipped/") {
+            fulfill(&match dir {
+                NORTH => [NORTH, SOUTHEAST],
+                SOUTH => [SOUTH, NORTHWEST],
+                EAST => [EAST, SOUTHWEST],
+                WEST => [NORTHEAST, WEST],
+                _ => return,
+            })
+        } else if atom.istype("/obj/structure/transit_tube/curved/") {
+            fulfill(&match dir {
+                NORTH => [NORTH, SOUTHWEST],
+                SOUTH => [SOUTH, NORTHEAST],
+                EAST => [EAST, NORTHWEST],
+                WEST => [SOUTHEAST, WEST],
+                _ => return,
+            })
+        } else if atom.istype("/obj/structure/transit_tube/diagonal/") {
+            fulfill(&match dir {
+                NORTH | SOUTH => [NORTHEAST, SOUTHWEST],
+                EAST | WEST => [NORTHWEST, SOUTHEAST],
+                _ => return,
+            })
+        } else {
+            fulfill(&match dir {
+                NORTH | SOUTH => [NORTH, SOUTH],
+                EAST | WEST => [EAST, WEST],
+                _ => return,
+            })
+        }
+    }
+}
+
+fn create_tube_overlay<'a>(output: &mut Vec<Atom<'a>>, objtree: &'a ObjectTree, source: &Atom<'a>, dir: i32, shift: i32) {
+    use dmi::*;
+
+    let mut copy = Atom::from_type(objtree, "/atom", source.loc).unwrap();
+    copy.set_var("dir", Constant::Int(dir));
+    copy.copy_var("layer", source, objtree);
+    copy.copy_var("icon", source, objtree);
+    if shift != 0 {
+        copy.set_var("icon_state", Constant::string("decorative_diag"));
+        match shift {
+            NORTH => copy.set_var("pixel_y", Constant::Int(32)),
+            SOUTH => copy.set_var("pixel_y", Constant::Int(-32)),
+            EAST => copy.set_var("pixel_x", Constant::Int(32)),
+            WEST => copy.set_var("pixel_x", Constant::Int(-32)),
+            _ => {}
+        }
+    } else {
+        copy.set_var("icon_state", Constant::string("decorative"));
+    }
+    output.push(copy);
 }
