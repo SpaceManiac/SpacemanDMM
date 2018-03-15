3ffeb036d4e5cbfd5329beac84452a7cc498c6f3
Tad Hardesty <tad@platymuus.com>
2018-03-14 21:47:33 -0700

    Allow limiting symbol queries to only types, procs, or vars

--- src/langserver/symbol_search.rs
+++ src/langserver/symbol_search.rs
@@ -5,23 +5,36 @@
 pub enum Query {
     Anything(String),
     Define(String),
+    Type(String),
+    Var(String),
+    Proc(String),
 }
 
 impl Query {
     /// Parse a symbol query.
     pub fn parse(query: &str) -> Option<Query> {
-        if query.is_empty() {
+        if query.is_empty() || query == "#" || query == "/" {
             return None
         }
-        if query.starts_with("#") {
-            let query = &query[1..];
+        Some(if query.starts_with("#") {
+            Query::Define(query[1..].to_lowercase())
+        } else if query.starts_with("var/") {
+            let query = &query["var/".len()..];
             if query.is_empty() {
                 return None
             }
-            return Some(Query::Define(query.to_lowercase()))
-        }
-
-        Some(Query::Anything(query.to_lowercase()))
+            Query::Var(query.to_lowercase())
+        } else if query.starts_with("proc/") {
+            let query = &query["proc/".len()..];
+            if query.is_empty() {
+                return None
+            }
+            Query::Proc(query.to_lowercase())
+        } else if query.contains("/") {
+            Query::Type(query.to_lowercase())
+        } else {
+            Query::Anything(query.to_lowercase())
+        })
     }
 
     /// Check whether this query matches the define with the given name.
@@ -29,34 +42,39 @@ impl Query {
         match *self {
             Query::Anything(ref q) => starts_with(name, q),
             Query::Define(ref q) => starts_with(name, q),
+            _ => false,
         }
     }
 
     pub fn matches_type(&self, name: &str, path: &str) -> bool {
         match *self {
-            Query::Anything(ref q) => starts_with(name, q)
-                || (q.contains("/") && path.to_lowercase().contains(q)),
+            Query::Anything(ref q) => starts_with(name, q),
+            Query::Type(ref q) => path.to_lowercase().contains(q),
             _ => false,
         }
     }
 
     pub fn matches_on_type(&self, _path: &str) -> bool {
         match *self {
-            Query::Anything(_) => true,
+            Query::Anything(_) |
+            Query::Proc(_) |
+            Query::Var(_) => true,
             _ => false,
         }
     }
 
     pub fn matches_var(&self, name: &str) -> bool {
         match *self {
-            Query::Anything(ref q) => starts_with(name, q),
+            Query::Anything(ref q) |
+            Query::Var(ref q) => starts_with(name, q),
             _ => false,
         }
     }
 
     pub fn matches_proc(&self, name: &str, _is_verb: bool) -> bool {
         match *self {
-            Query::Anything(ref q) => starts_with(name, q),
+            Query::Anything(ref q) |
+            Query::Proc(ref q) => starts_with(name, q),
             _ => false,
         }
     }
