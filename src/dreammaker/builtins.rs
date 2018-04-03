ac75b50b439f63bd4da852fd9cdc7f734b51c761
Tad Hardesty <tad@platymuus.com>
2018-04-02 23:10:41 -0700

    Move built-in defines to the builtins file

--- src/dreammaker/builtins.rs
+++ src/dreammaker/builtins.rs
@@ -1,8 +1,110 @@
 //! BYOND built-in types, procs, and vars.
 
+use std::collections::HashMap;
+
 use super::objtree::*;
 use super::ast::*;
 use super::{Location, FileId, DMError};
+use super::preprocessor::Define;
+
+/// Register BYOND builtin macros to the given define map.
+pub fn default_defines(defines: &mut HashMap<String, Define>) {
+    use super::lexer::Token::*;
+
+    macro_rules! c {
+        ($($i:ident = $($x:expr),*;)*) => {
+            $(defines.insert(stringify!($i).into(), Define::Constant { subst: vec![$($x),*] });)*
+        }
+    }
+    c! {
+        DM_VERSION = Int(511);
+
+        FALSE = Int(0);
+        TRUE = Int(1);
+
+        NORTH = Int(1);
+        SOUTH = Int(2);
+        EAST = Int(4);
+        WEST = Int(8);
+        NORTHEAST = Int(5);
+        SOUTHEAST = Int(6);
+        NORTHWEST = Int(9);
+        SOUTHWEST = Int(10);
+
+        FLOAT_LAYER = Int(-1);
+        AREA_LAYER = Int(1);
+        TURF_LAYER = Int(2);
+        OBJ_LAYER = Int(3);
+        MOB_LAYER = Int(4);
+        FLY_LAYER = Int(5);
+        EFFECTS_LAYER = Int(5000);
+        TOPDOWN_LAYER = Int(10000);
+        BACKGROUND_LAYER = Int(20000);
+
+        ICON_ADD = Int(0);
+        ICON_SUBTRACT = Int(1);
+        ICON_MULTIPLY = Int(2);
+        ICON_OVERLAY = Int(3);
+        ICON_AND = Int(4);
+        ICON_OR = Int(5);
+        ICON_UNDERLAY = Int(6);
+
+        BLEND_DEFAULT = Int(0);
+        BLEND_OVERLAY = Int(1);
+        BLEND_ADD = Int(2);
+        BLEND_SUBTRACT = Int(3);
+        BLEND_MULTIPLY = Int(4);
+
+        NO_STEPS = Int(0);
+        FORWARD_STEPS = Int(1);
+        SLIDE_STEPS = Int(2);
+        SYNC_STEPS = Int(3);
+
+        BLIND = Int(1);
+        SEE_MOBS = Int(4);
+        SEE_OBJS = Int(8);
+        SEE_TURFS = Int(16);
+        SEE_SELF = Int(32);
+        SEE_INFRA = Int(64);
+        SEE_PIXELS = Int(256);
+        SEE_THRU = Int(512);
+        SEE_BLACKNESS = Int(1024);
+
+        LONG_GLIDE = Int(1);
+        RESET_COLOR = Int(2);
+        RESET_ALPHA = Int(4);
+        RESET_TRANSFORM = Int(8);
+        NO_CLIENT_COLOR = Int(16);
+        KEEP_TOGETHER = Int(32);
+        KEEP_APART = Int(64);
+        PLANE_MASTER = Int(128);
+        TILE_BOUND = Int(256);
+        PIXEL_SCALE = Int(512);
+
+        TOPDOWN_MAP = Int(0);
+        ISOMETRIC_MAP = Int(1);
+        SIDE_MAP = Int(2);
+        TILED_ICON_MAP = Int(32768);
+
+        CONTROL_FREAK_ALL = Int(1);
+        CONTROL_FREAK_SKIN = Int(2);
+        CONTROL_FREAK_MACROS = Int(4);
+
+        MOB_PERSPECTIVE = Int(0);
+        EYE_PERSPECTIVE = Int(1);
+        EDGE_PERSPECTIVE = Int(2);
+
+        MOUSE_ACTIVE_POINTER = Int(1);
+
+        MS_WINDOWS = String("MS Windows".into());
+        UNIX = String("UNIX".into());
+        MALE = String("male".into());
+        FEMALE = String("female".into());
+        NEUTER = String("neuter".into());
+        PLURAL = String("plural".into());
+    }
+    // TODO: functions: ASSERT, CRASH, EXCEPTION
+}
 
 /// Register BYOND builtins into the specified object tree.
 pub fn register_builtins(tree: &mut ObjectTree) -> Result<(), DMError> {
