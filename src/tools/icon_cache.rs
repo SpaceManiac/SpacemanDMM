e5b25cf338353d7b179155720e742706596247e4
Tad Hardesty <tad@platymuus.com>
2018-06-08 14:39:26 -0700

    Only show an error the first time for an icon

--- src/tools/icon_cache.rs
+++ src/tools/icon_cache.rs
@@ -6,28 +6,28 @@ use super::dmi::IconFile;
 
 #[derive(Default)]
 pub struct IconCache {
-    lock: RwLock<HashMap<PathBuf, Arc<IconFile>>>,
+    lock: RwLock<HashMap<PathBuf, Option<Arc<IconFile>>>>,
 }
 
 impl IconCache {
     pub fn retrieve_uniq(&mut self, path: &Path) -> Option<&IconFile> {
         let map = self.lock.get_mut().unwrap();
-        match map.entry(path.to_owned()) {
-            hash_map::Entry::Occupied(entry) => Some(entry.into_mut()),
-            hash_map::Entry::Vacant(entry) => load(path).map(|icon| &**entry.insert(Arc::new(icon))),
-        }
+        (match map.entry(path.to_owned()) {
+            hash_map::Entry::Occupied(entry) => entry.into_mut().as_mut(),
+            hash_map::Entry::Vacant(entry) => entry.insert(load(path).map(Arc::new)).as_mut(),
+        }.map(|x| &**x))
     }
 
     pub fn retrieve_shared(&self, path: &Path) -> Option<Arc<IconFile>> {
         let existing = self.lock.read().unwrap().get(path).cloned();
         // shouldn't be inlined or the lifetime of the lock will be extended
         match existing {
-            Some(existing) => Some(existing),
-            None => load(path).map(|icon| {
-                let arc = Arc::new(icon);
+            Some(existing) => existing,
+            None => {
+                let arc = load(path).map(Arc::new);
                 self.lock.write().unwrap().insert(path.to_owned(), arc.clone());
                 arc
-            }),
+            },
         }
     }
 }
@@ -36,7 +36,7 @@ fn load(path: &Path) -> Option<IconFile> {
     match IconFile::from_file(path) {
         Ok(loaded) => Some(loaded),
         Err(err) => {
-            eprintln!("error loading icon: {}\n{}", path.display(), err);
+            eprintln!("error loading icon: {}\n  {}", path.display(), err);
             None
         }
     }
