c85405813955103cb2d7a28c605b1d02a986451e
Tad Hardesty <tad@platymuus.com>
2018-08-25 02:37:07 -0700

    Add imgui hello world example

--- src/editor2/main.rs
+++ src/editor2/main.rs
@@ -1,5 +1,35 @@
 //! The map editor proper, with a GUI and everything.
 
-fn main() {
+extern crate glium;
+#[macro_use] extern crate imgui;
+extern crate imgui_glium_renderer;
+extern crate dreammaker as dm;
 
+mod support;
+
+pub use glium::glutin;
+use imgui::*;
+
+const CLEAR_COLOR: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+
+fn main() {
+    support::run("SpacemanDMM".to_owned(), CLEAR_COLOR, hello_world);
+}
+
+fn hello_world<'a>(ui: &Ui<'a>) -> bool {
+    ui.window(im_str!("Hello world"))
+        .size((300.0, 100.0), ImGuiCond::FirstUseEver)
+        .build(|| {
+            ui.text(im_str!("Hello world!"));
+            ui.text(im_str!("This...is...imgui-rs!"));
+            ui.separator();
+            let mouse_pos = ui.imgui().mouse_pos();
+            ui.text(im_str!(
+                "Mouse Position: ({:.1},{:.1})",
+                mouse_pos.0,
+                mouse_pos.1
+            ));
+        });
+
+    true
 }
