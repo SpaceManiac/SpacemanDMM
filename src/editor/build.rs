87f0f4babfd3d10980a18e03ad80991239685ab4
Tad Hardesty <tad@platymuus.com>
2018-08-26 20:15:00 -0700

    Warn rather than error on missing windres

--- src/editor/build.rs
+++ src/editor/build.rs
@@ -5,23 +5,31 @@ use std::path::Path;
 fn main() {
     if cfg!(windows) {
         let out_dir = env::var("OUT_DIR").ok().expect("can't find out_dir");
-        println!("cargo:rerun-if-changed=editor.rc");
-        println!("cargo:rerun-if-changed=gasmask.ico");
-        println!("cargo:rustc-link-search=native={}", out_dir);
 
         if cfg!(target_env="msvc") {
-            Command::new("windres").args(&["editor.rc",  "-o"])
+            if let Err(e) = Command::new("windres").args(&["editor.rc",  "-o"])
                 .arg(&format!("{}/editor_rc.lib", out_dir))
-                .status().unwrap();
+                .status()
+            {
+                println!("cargo:warning=`windres` unavailable: {}", e);
+                return;
+            }
         } else {
-            Command::new("windres").args(&["editor.rc",  "-o"])
+            if let Err(e) = Command::new("windres").args(&["editor.rc",  "-o"])
                 .arg(&format!("{}/editor.rc.o", out_dir))
-                .status().unwrap();
+                .status()
+            {
+                println!("cargo:warning=`windres` unavailable: {}", e);
+                return;
+            }
             Command::new("ar").args(&["crus", "libeditor_rc.a", "editor.rc.o"])
                 .current_dir(Path::new(&out_dir))
                 .status().unwrap();
         }
 
+        println!("cargo:rerun-if-changed=editor.rc");
+        println!("cargo:rerun-if-changed=gasmask.ico");
+        println!("cargo:rustc-link-search=native={}", out_dir);
         println!("cargo:rustc-link-lib=static=editor_rc");
     }
 }
