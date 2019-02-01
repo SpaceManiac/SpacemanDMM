6bb747027c3334c6726d5a29fd7bad8250e617b1
Tad Hardesty <tad@platymuus.com>
2019-02-01 13:46:19 -0800

    Copy lint example to DreamChecker as a starting point

--- src/dreamchecker/main.rs
+++ src/dreamchecker/main.rs
@@ -1,6 +1,18 @@
 //! DreamChecker, a robust static analysis and typechecking engine for
 //! DreamMaker.
 
+extern crate dreammaker as dm;
+
 fn main() {
-    println!("Hello, world!");
+    let mut context = dm::Context::default();
+    context.set_print_severity(Some(dm::Severity::Error));
+    let env = dm::detect_environment_default()
+        .expect("error detecting .dme")
+        .expect("no .dme found");
+    let pp = dm::preprocessor::Preprocessor::new(&context, env)
+        .expect("i/o error opening .dme");
+    let indents = dm::indents::IndentProcessor::new(&context, pp);
+    let mut parser = dm::parser::Parser::new(&context, indents);
+    parser.enable_procs();
+    parser.parse_object_tree();
 }
