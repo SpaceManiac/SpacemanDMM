8c93659e7ce6cb0ec4ab05bb4ec693fbc4c7cb82
Tad Hardesty <tad@platymuus.com>
2019-02-04 00:57:34 -0800

    Stub out DreamChecker further

--- src/dreamchecker/main.rs
+++ src/dreamchecker/main.rs
@@ -2,6 +2,15 @@
 //! DreamMaker.
 
 extern crate dreammaker as dm;
+use dm::objtree::{ProcValue, Code};
+use dm::ast::Statement;
+
+fn some_analysis(func: &ProcValue, code: &[Statement]) {
+    println!("{:?}", func.location);
+    for statement in code {
+        println!("{:?}", statement);
+    }
+}
 
 fn main() {
     let mut context = dm::Context::default();
@@ -14,5 +23,29 @@ fn main() {
     let indents = dm::indents::IndentProcessor::new(&context, pp);
     let mut parser = dm::parser::Parser::new(&context, indents);
     parser.enable_procs();
-    parser.parse_object_tree();
+    let tree = parser.parse_object_tree();
+
+    let mut present = 0;
+    let mut invalid = 0;
+    let mut builtin = 0;
+    let mut disabled = 0;
+
+    tree.root().recurse(&mut |ty| {
+        for proc in ty.procs.values() {
+            for value in proc.value.iter() {
+                match value.code {
+                    Code::Present(ref code) => {
+                        present += 1;
+                        some_analysis(&value, code);
+                        std::process::exit(0);
+                    }
+                    Code::Invalid(_) => invalid += 1,
+                    Code::Builtin => builtin += 1,
+                    Code::Disabled => disabled += 1,
+                }
+            }
+        }
+    });
+
+    println!("{:?}", (present, invalid, builtin, disabled));
 }
