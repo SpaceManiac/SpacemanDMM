d2e836ba9238e96e04c1831c18dac5c47d2b5408
Tad Hardesty <tad@platymuus.com>
2019-03-03 00:54:48 -0800

    Add tests for the ternary operator

---
+++ src/dreammaker/tests/expression_tests.rs
@@ -0,0 +1,79 @@
+extern crate dreammaker as dm;
+
+use dm::lexer::Lexer;
+use dm::parser::*;
+use dm::ast::*;
+
+fn parse_expr(f: &str) -> Expression {
+    let context = Default::default();
+    let lexer = Lexer::new(&context, Default::default(), f.bytes().map(Ok));
+    let mut parser = Parser::new(&context, lexer);
+    let result = parser.require_expression().expect("failed to parse expression");
+    context.assert_success();
+    result
+}
+
+#[test]
+fn ternary_precedence() {
+    // (foo = ((1 + 2) ? (3 + 4) : (5 + 6))
+    assert_eq!(
+        parse_expr("foo = 1 + 2 ? 3 + 4 : 5 + 6"),
+        Expression::AssignOp {
+            op: AssignOp::Assign,
+            lhs: Box::new(Expression::from(Term::Ident("foo".to_owned()))),
+            rhs: Box::new(Expression::TernaryOp {
+                cond: Box::new(Expression::BinaryOp {
+                    op: BinaryOp::Add,
+                    lhs: Box::new(Expression::from(Term::Int(1))),
+                    rhs: Box::new(Expression::from(Term::Int(2))),
+                }),
+                if_: Box::new(Expression::BinaryOp {
+                    op: BinaryOp::Add,
+                    lhs: Box::new(Expression::from(Term::Int(3))),
+                    rhs: Box::new(Expression::from(Term::Int(4))),
+                }),
+                else_: Box::new(Expression::BinaryOp {
+                    op: BinaryOp::Add,
+                    lhs: Box::new(Expression::from(Term::Int(5))),
+                    rhs: Box::new(Expression::from(Term::Int(6))),
+                }),
+            }),
+        }
+    )
+}
+
+#[test]
+fn ternary_nesting() {
+    // 1 ? 2 : (3 ? 4 : 5)
+    assert_eq!(
+        parse_expr("1 ? 2 : 3 ? 4 : 5"),
+        Expression::TernaryOp {
+            cond: Box::new(Expression::from(Term::Int(1))),
+            if_: Box::new(Expression::from(Term::Int(2))),
+            else_: Box::new(Expression::TernaryOp {
+                cond: Box::new(Expression::from(Term::Int(3))),
+                if_: Box::new(Expression::from(Term::Int(4))),
+                else_: Box::new(Expression::from(Term::Int(5))),
+            }),
+        }
+    );
+    // 1 ? (2 ? 3 : 4) : 5
+    assert_eq!(
+        parse_expr("1 ? 2 ? 3 : 4 : 5"),
+        Expression::TernaryOp {
+            cond: Box::new(Expression::from(Term::Int(1))),
+            if_: Box::new(Expression::TernaryOp {
+                cond: Box::new(Expression::from(Term::Int(2))),
+                if_: Box::new(Expression::from(Term::Int(3))),
+                else_: Box::new(Expression::from(Term::Int(4))),
+            }),
+            else_: Box::new(Expression::from(Term::Int(5))),
+        }
+    );
+}
+
+#[test]
+fn ternary_without_spaces() {
+    parse_expr(r#"listkey = set_keyword ? "[set_keyword] [locname]":"[locname]""#);
+    parse_expr(r#"pump_direction?("release"):("siphon")"#);
+}
