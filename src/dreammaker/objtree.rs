9b3976e0432bd78a756a48f48fc48db46d3b1cc4
Tad Hardesty <tad@platymuus.com>
2019-03-19 17:14:42 -0700

    Downgrade 'procedure override precedes definition' to a warning

--- src/dreammaker/objtree.rs
+++ src/dreammaker/objtree.rs
@@ -12,7 +12,7 @@ use linked_hash_map::LinkedHashMap;
 use super::ast::{Expression, VarType, VarSuffix, PathOp, Parameter, Block, ProcDeclKind};
 use super::constants::{Constant, Pop};
 use super::docs::DocCollection;
-use super::{DMError, Location, Context};
+use super::{DMError, Location, Context, Severity};
 
 // ----------------------------------------------------------------------------
 // Symbol IDs
@@ -917,6 +917,7 @@ impl ObjectTree {
             // Show the error now, make up for it by putting the original
             // at the beginning of the list (so `..()` finds it).
             DMError::new(proc.value[0].location, format!("procedure override precedes definition"))
+                .set_severity(Severity::Warning)
                 .with_note(location, "definition is here")
                 .register(context);
             proc.value.insert(0, value);
