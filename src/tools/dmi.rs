a8d002d6a66a88cf36cccba6116a36f4e990ebab
Tad Hardesty <tad@platymuus.com>
2019-11-17 14:50:20 -0800

    Remove duplicate constants from dmm_tools::dmi

--- src/tools/dmi.rs
+++ src/tools/dmi.rs
@@ -6,18 +6,9 @@ use ndarray::Array3;
 use lodepng::{self, RGBA};
 use lodepng::ffi::{State as PngState, ColorType};
 
-pub const NORTH: i32 = 1;
-pub const SOUTH: i32 = 2;
-pub const EAST: i32 = 4;
-pub const WEST: i32 = 8;
-pub const NORTHEAST: i32 = 5;
-pub const NORTHWEST: i32 = 9;
-pub const SOUTHEAST: i32 = 6;
-pub const SOUTHWEST: i32 = 10;
+pub use dm::dmi::*;
 
 type Rect = (u32, u32, u32, u32);
-
-pub use dm::dmi::*;
 
 // ----------------------------------------------------------------------------
 // Icon file and metadata handling
