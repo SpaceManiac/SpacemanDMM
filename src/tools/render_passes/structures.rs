7f8673b46747a2a9ec62c13961c08249218c0281
Tad Hardesty <tad@platymuus.com>
2019-11-16 16:27:15 -0800

    Avoid heap-allocation in gravity generator expansion

--- src/tools/render_passes/structures.rs
+++ src/tools/render_passes/structures.rs
@@ -49,19 +49,19 @@ impl RenderPass for GravityGen {
             return false;
         }
 
-        for &(count, x, y) in &[
-            (1, 1, -2),
-            (2, 0, -2),
-            (3, -1, -2),
-            (4, 1, -1),
-            (5, 0, -1),
-            (6, -1, -1),
-            (7, 1, 0),
-            (9, -1, 0),
+        for &(count, icon_state, x, y) in &[
+            (1, "on_1", 1, -2),
+            (2, "on_2", 0, -2),
+            (3, "on_3", -1, -2),
+            (4, "on_4", 1, -1),
+            (5, "on_5", 0, -1),
+            (6, "on_6", -1, -1),
+            (7, "on_7", 1, 0),
+            (9, "on_9", -1, 0),
         ] {
             let new_loc = ((atom.loc.0 as i32 + x) as u32, (atom.loc.1 as i32 + y) as u32);
             let mut new_atom = Atom::from_type(objtree, "/obj/machinery/gravity_generator/part", new_loc).unwrap();
-            new_atom.set_var("icon_state", Constant::string(format!("on_{}", count)));
+            new_atom.set_var("icon_state", Constant::string(icon_state));
             if count <= 3 {
                 new_atom.set_var("density", Constant::Int(0));
                 new_atom.set_var("layer", Constant::from(4.25));  // WALL_OBJ_LAYER
