e6c9f944a01901059ff51979c332ba2d59723b91
Tad Hardesty <tad@platymuus.com>
2019-11-28 16:54:16 -0800

    Use debug_output in DAP NYI

--- src/langserver/macros.rs
+++ src/langserver/macros.rs
@@ -110,7 +110,7 @@ macro_rules! handle_request {
                     let result: <$what as Request>::Result = self.$what(params)?;
                     Ok(serde_json::to_value(result).expect("encode problem"))
                 } else)* {
-                    eprintln!("Request NYI: {} -> {:?}", request.command, request.arguments);
+                    debug_output!(in self.seq, "[main] NYI: {} -> {:?}", request.command, request.arguments);
                     Err(format!("Request NYI: {}", request.command).into())
                 }
             }
