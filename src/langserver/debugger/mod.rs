8cb0491834e3383d6868081cf08ecfabb5c2ef98
Tad Hardesty <tad@platymuus.com>
2019-12-07 16:29:13 -0800

    Disable extools by default in release builds

--- src/langserver/debugger/mod.rs
+++ src/langserver/debugger/mod.rs
@@ -280,7 +280,8 @@ handle_request! {
     }
 
     on LaunchVsc(&mut self, params) {
-        let debug = !params.base.noDebug.unwrap_or(false);
+        // Extools is not currently enabled on release langserver builds.
+        let debug = cfg!(debug_assertions) && !params.base.noDebug.unwrap_or(false);
         self.launched = Some(Launched::new(self.seq.clone(), &self.dreamseeker_exe, &params.dmb, debug)?);
         if debug {
             self.extools = Extools::connect(self.seq.clone(), extools::DEFAULT_PORT)?;
