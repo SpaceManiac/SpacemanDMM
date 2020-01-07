7c0059d21deb648a85e68011703b8394ea83ceaf
steamport <steamport@tutanota.com>
2020-01-06 20:55:20 -0500

    Add /client/proc/SoundQuery to builtins (#139)

--- src/dreammaker/builtins.rs
+++ src/dreammaker/builtins.rs
@@ -781,6 +781,7 @@ pub fn register_builtins(tree: &mut ObjectTree) -> Result<(), DMError> {
         client/proc/Northeast();
         client/proc/Northwest();
         client/proc/SendPage(msg, recipient, options);
+        client/proc/SoundQuery();
         client/proc/South();
         client/proc/Southeast();
         client/proc/Southwest();
