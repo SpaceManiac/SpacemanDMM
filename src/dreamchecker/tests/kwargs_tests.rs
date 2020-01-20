04783eb17de8159c352f12f449ee06f7d918b533
spookydonut <github@spooksoftware.com>
2020-01-20 15:16:50 +0800

    kwarg

---
+++ src/dreamchecker/tests/kwargs_tests.rs
@@ -0,0 +1,18 @@
+
+extern crate dreamchecker as dc;
+
+use dc::test_helpers::*;
+
+pub const AFTER_KWARG_ERRORS: &[(u32, u16, &str)] = &[
+    (3, 5, "proc called with non-kwargs after kwargs: foo()"),
+];
+
+#[test]
+fn after_kwarg() {
+    let code = r##"
+/proc/foo(arg1, arg2, arg3)
+/proc/test()
+    foo(arg2=1, 1)
+"##.trim();
+    check_errors_match(code, AFTER_KWARG_ERRORS);
+}
