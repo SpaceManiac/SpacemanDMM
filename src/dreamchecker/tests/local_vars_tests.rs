1b43fbcce29d9650c2804bb58877de0f4c71868a
spookydonut <github@spooksoftware.com>
2020-03-04 16:46:20 +0800

    Make local vars locally scoped (#130)
    * makes local vars locally scoped
    
    * tests

---
+++ src/dreamchecker/tests/local_vars_tests.rs
@@ -0,0 +1,30 @@
+
+extern crate dreamchecker as dc;
+
+use dc::test_helpers::check_errors_match;
+
+pub const LOCAL_SCOPE_ERRORS: &[(u32, u16, &str)] = &[
+    (7, 9, "undefined var: \"bar\""),
+    (9, 5, "undefined var: \"bar\""),
+    (13, 5, "undefined var: \"bar\""),
+];
+
+#[test]
+fn local_scope() {
+    let code = r##"
+/proc/test()
+    var/foo
+    if(prob(50))
+        var/bar
+        foo++
+    else
+        bar++
+        foo++
+    bar++
+    foo++
+    alabel:
+        var/bar
+    bar++
+"##.trim();
+    check_errors_match(code, LOCAL_SCOPE_ERRORS);
+}
