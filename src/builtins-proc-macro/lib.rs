9f45b40eb1e354bbcc6beba146d9b38b7f15fc6e
Tad Hardesty <tad@platymuus.com>
2020-12-13 14:19:54 -0800

    Convert builtin attributes to .docs method calls

--- src/builtins-proc-macro/lib.rs
+++ src/builtins-proc-macro/lib.rs
@@ -1,3 +1,4 @@
+use export::TokenStream2;
 use proc_macro::TokenStream;
 use quote::{quote, quote_spanned};
 use syn::*;
@@ -5,6 +6,7 @@ use syn::ext::IdentExt;
 use syn::parse::{Parse, ParseStream};
 use syn::token::Paren;
 use syn::punctuated::Punctuated;
+use syn::spanned::Spanned;
 
 struct ProcArgument {
     name: Ident,
@@ -30,13 +32,14 @@ enum EntryBody {
 }
 
 struct BuiltinEntry {
+    attrs: Vec<Attribute>,
     path: Vec<Ident>,
     body: EntryBody,
 }
 
 impl Parse for BuiltinEntry {
     fn parse(input: ParseStream) -> Result<Self> {
-        let _attrs = Attribute::parse_outer(input)?;
+        let attrs = Attribute::parse_outer(input)?;
 
         let ident = input.parse()?;
         let mut path = vec![ident];
@@ -60,6 +63,7 @@ impl Parse for BuiltinEntry {
 
         input.parse::<Token![;]>()?.span;
         Ok(BuiltinEntry {
+            attrs,
             path,
             body,
         })
@@ -77,6 +81,15 @@ impl Parse for Builtins {
     }
 }
 
+struct DocComment(LitStr);
+impl Parse for DocComment {
+    fn parse(input: ParseStream) -> Result<Self> {
+        input.parse::<Token![=]>()?;
+        let lit = input.parse::<LitStr>()?;
+        Ok(DocComment(lit))
+    }
+}
+
 #[proc_macro]
 pub fn builtins_table(input: TokenStream) -> TokenStream {
     let builtins = parse_macro_input!(input as Builtins).0;
@@ -89,31 +102,54 @@ pub fn builtins_table(input: TokenStream) -> TokenStream {
             &[ #(#lit_strs),* ]
         };
 
+        let mut markdown = String::new();
+        let mut markdown_span = None;
+
+        let mut attr_calls = TokenStream2::new();
+        for attr in entry.attrs {
+            let attr_span = attr.span();
+            let path = attr.path;
+            let ident = &path.segments.last().unwrap().ident;
+            if ident == "doc" {
+                markdown_span = Some(attr_span);
+                markdown.push_str(&syn::parse2::<DocComment>(attr.tokens).unwrap().0.value());
+                markdown.push_str("\n");
+            } else {
+                attr_calls.extend(quote_spanned! { attr_span => .docs.#path });
+                attr_calls.extend(attr.tokens);
+            }
+        }
+
+        if let Some(markdown_span) = markdown_span {
+            let lit = LitStr::new(&markdown, span);
+            attr_calls.extend(quote_spanned! { markdown_span => .docs.doc(#lit) });
+        }
+
         let line = match entry.body {
             EntryBody::None => {
                 quote_spanned! { span =>
-                    tree.add_builtin_type(#path);
+                    tree.add_builtin_type(#path) #attr_calls;
                 }
             },
             EntryBody::Variable(None) => {
                 quote_spanned! { span =>
-                    tree.add_builtin_var(#path, None);
+                    tree.add_builtin_var(#path, None) #attr_calls;
                 }
             },
             EntryBody::Variable(Some(expr)) => {
                 quote_spanned! { span =>
-                    tree.add_builtin_var(#path, Some(#expr));
+                    tree.add_builtin_var(#path, Some(#expr)) #attr_calls;
                 }
             },
             EntryBody::Proc(args) => {
                 let args: Vec<_> = args.into_iter().map(|x| LitStr::new(&x.name.to_string(), x.name.span())).collect();
                 quote_spanned! { span =>
-                    tree.add_builtin_proc(#path, &[ #(#args),* ]);
+                    tree.add_builtin_proc(#path, &[ #(#args),* ]) #attr_calls;
                 }
             }
         };
-        output.push(TokenStream::from(line));
+        output.push(line);
     }
 
-    output.into_iter().flat_map(|x| x).collect()
+    output.into_iter().flat_map(|x| TokenStream::from(x)).collect()
 }
