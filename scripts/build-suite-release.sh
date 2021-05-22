212ff9aa482a42524f9ecc336bdc1235240e9281
Tad Hardesty <tad@platymuus.com>
2021-05-22 14:06:18 -0700

    Rename prepare-binaries.sh to build-suite-release

---
+++ scripts/build-suite-release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+relname=$(git describe --tags --exact)
+echo "Using tag name: $relname"
+
+# -----------------------------------------------------------------------------
+# Cargo build
+touch src/*/build.rs
+
+echo '==== Linux build ===='
+cargo build --release --target x86_64-unknown-linux-musl
+
+echo '==== Windows build ===='
+cargo build --release --target x86_64-pc-windows-gnu
+
+# -----------------------------------------------------------------------------
+# Organize binaries into a directory for upload
+echo '==== Organize files ===='
+DEST=target/dist
+rm -rf "$DEST"
+mkdir -p "$DEST"
+cp \
+    target/x86_64-unknown-linux-musl/release/{dreamchecker,dmdoc,dmm-tools,dm-langserver} \
+    target/x86_64-pc-windows-gnu/release/{dreamchecker,dmdoc,dmm-tools,dm-langserver}.exe \
+    "$DEST"
+echo "# SpacemanDMM $relname" | tee "$DEST/$relname.sha256"
+sha256sum "$DEST"/* | tee -a "$DEST/$relname.sha256"
+gpg2 --sign --armor --detach "$DEST/$relname.sha256"
+gpg2 --verify "$DEST/$relname.sha256.asc"
+ls -lh --color=auto "$DEST"
