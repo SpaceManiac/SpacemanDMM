d675b9ab5613a288ab5ef42c52837dd8527b9e09
pali <6pali6@gmail.com>
2022-02-07 19:06:39 +0100

    cargo fmt

--- crates/dreamchecker/src/switch_rand_range.rs
+++ crates/dreamchecker/src/switch_rand_range.rs
@@ -1,24 +1,22 @@
-
 use std::borrow::Borrow;
 
-use dm::{Context, DMError, Location, Severity};
 use dm::ast::*;
+use dm::{Context, DMError, Location, Severity};
 
 /**
  * Checks for mistakes in switches of the form `switch(rand(L, H))`.
  * If some cases lie outside of the [L, H] range or the whole [L, H] range is not covered by all the cases a warning is issued.
  */
 pub fn check_switch_rand_range(
-        input: &Box<Expression>,
-        cases: &Box<[(Spanned<Vec<Case>>, Block)]>,
-        default: &Option<Block>,
-        location: Location,
-        context: &Context,
-    ) {
+    input: &Box<Expression>,
+    cases: &Box<[(Spanned<Vec<Case>>, Block)]>,
+    default: &Option<Block>,
+    location: Location,
+    context: &Context,
+) {
     let (rand_start, rand_end) = if let Some(range) = get_rand_range(input) {
         range
-    }
-    else {
+    } else {
         return;
     };
 
@@ -32,8 +30,7 @@ pub fn check_switch_rand_range(
                 let end = end.floor() as i32;
                 if start <= rand_end && end >= rand_start {
                     case_ranges.push((start, end));
-                }
-                else {
+                } else {
                     DMError::new(location, format!("Case range '{} to {}' will never trigger as it is outside the rand() range {} to {}", start, end, rand_start, rand_end))
                         .with_component(dm::Component::DreamChecker)
                         .set_severity(Severity::Warning)
@@ -43,7 +40,8 @@ pub fn check_switch_rand_range(
         }
     }
 
-    if default.is_some() { // covers the whole range directly so no need to check for gaps
+    if default.is_some() {
+        // covers the whole range directly so no need to check for gaps
         return;
     }
 
@@ -52,60 +50,66 @@ pub fn check_switch_rand_range(
     for (start, end) in case_ranges.iter() {
         if *start > first_uncovered {
             break;
-        }
-        else {
+        } else {
             first_uncovered = std::cmp::max(first_uncovered, end + 1);
         }
     }
 
     if first_uncovered <= rand_end {
-        DMError::new(location, format!("Switch branches on rand() with range {} to {} but no case branch triggers for {}", rand_start, rand_end, first_uncovered))
-            .with_component(dm::Component::DreamChecker)
-            .set_severity(Severity::Warning)
-            .register(context);
+        DMError::new(
+            location,
+            format!(
+                "Switch branches on rand() with range {} to {} but no case branch triggers for {}",
+                rand_start, rand_end, first_uncovered
+            ),
+        )
+        .with_component(dm::Component::DreamChecker)
+        .set_severity(Severity::Warning)
+        .register(context);
     }
 }
 
 fn get_case_range(case: &Case, location: Location) -> Option<(f32, f32)> {
     match case {
         Case::Exact(ref value) => {
-            let value = value.to_owned().simple_evaluate(location).ok()?.to_float()?;
+            let value = value
+                .to_owned()
+                .simple_evaluate(location)
+                .ok()?
+                .to_float()?;
             Some((value, value))
-        },
+        }
         Case::Range(ref min, ref max) => {
             let min = min.to_owned().simple_evaluate(location).ok()?.to_float()?;
             let max = max.to_owned().simple_evaluate(location).ok()?.to_float()?;
             Some((min, max))
-        },
+        }
     }
 }
 
 fn get_rand_range(maybe_rand: &Expression) -> Option<(i32, i32)> {
     let (term, location) = match maybe_rand {
-        &Expression::Base { ref term, ref follow } if follow.is_empty() => (&term.elem, term.location),
+        &Expression::Base {
+            ref term,
+            ref follow,
+        } if follow.is_empty() => (&term.elem, term.location),
         _ => return None,
     };
 
     let rand_args: &[Expression] = match term {
-        Term::Call(proc_name, args) if proc_name.as_str() == "rand" => {
-            args.borrow()
-        }
+        Term::Call(proc_name, args) if proc_name.as_str() == "rand" => args.borrow(),
         _ => return None,
     };
 
     let (min, max) = match rand_args {
-        [min, max] => {
-            (
-                min.to_owned().simple_evaluate(location).ok()?.to_float()?,
-                max.to_owned().simple_evaluate(location).ok()?.to_float()?
-            )
-        },
-        [max] => {
-            (
-                0.,
-                max.to_owned().simple_evaluate(location).ok()?.to_float()?
-            )
-        },
+        [min, max] => (
+            min.to_owned().simple_evaluate(location).ok()?.to_float()?,
+            max.to_owned().simple_evaluate(location).ok()?.to_float()?,
+        ),
+        [max] => (
+            0.,
+            max.to_owned().simple_evaluate(location).ok()?.to_float()?,
+        ),
         _ => return None,
     };
 
