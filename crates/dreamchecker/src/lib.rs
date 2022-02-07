7c49d05c38bce90debf168917051fec341ac47ad
pali <6pali6@gmail.com>
2022-02-07 21:46:39 +0100

    removed unnecessary !format I'm dumb

--- crates/dreamchecker/src/lib.rs
+++ crates/dreamchecker/src/lib.rs
@@ -1484,7 +1484,7 @@ impl<'o, 's> AnalyzeProc<'o, 's> {
                 for (case, ref block) in cases.iter() {
                     let mut scoped_locals = local_vars.clone();
                     if let [dm::ast::Case::Exact(Expression::BinaryOp{op: BinaryOp::Or, ..})] = case.elem[..] {
-                        error(case.location, format!("Elements in a switch-case branch separated by ||, this is likely in error and should be replaced by a comma"))
+                        error(case.location, "Elements in a switch-case branch separated by ||, this is likely in error and should be replaced by a comma")
                             .set_severity(Severity::Warning)
                             .register(self.context);
                     }
