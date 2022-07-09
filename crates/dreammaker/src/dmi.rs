d4c3d3f6e30ca5022c64aae4129510e999584215
tigercat2000 <nick.pilant2@gmail.com>
2022-07-09 15:03:45 -0700

    Only show (number) in StateIndex::Display if duplicate count is >0

--- crates/dreammaker/src/dmi.rs
+++ crates/dreammaker/src/dmi.rs
@@ -16,7 +16,11 @@ pub struct StateIndex(String, u32);
 
 impl Display for StateIndex {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} ({})", self.0, self.1)
+        if self.1 == 0 {
+            write!(f, "{}", self.0)
+        } else {
+            write!(f, "{} ({})", self.0, self.1)
+        }
     }
 }
 
