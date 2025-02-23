9fd698e2d63ec86534c81ef1dd82f652cbe6109c
Tad Hardesty <tad@platymuus.com>
2025-02-23 14:36:22 -0800

    Update to auxtools debug server v2.3.4

--- crates/dm-langserver/build.rs
+++ crates/dm-langserver/build.rs
@@ -32,9 +32,9 @@ fn main() {
     download_dll(
         &out_dir,
         "debug_server.dll",
-        "v2.3.3", // DEBUG_SERVER_TAG
-        "https://github.com/willox/auxtools/releases/download/v2.3.3/debug_server.dll", // DEBUG_SERVER_DLL_URL
-        "8c3633d8237738be39a8c8f34bbd3d819cdaa270d1f30774c5007481fc32418c", // DEBUG_SERVER_DLL_SHA256
+        "v2.3.4", // DEBUG_SERVER_TAG
+        "https://github.com/willox/auxtools/releases/download/v2.3.4/debug_server.dll", // DEBUG_SERVER_DLL_URL
+        "06547804cf39c74d6a74aa1663a47f438871ea8fa85deb7dcb0c8d772747db7c", // DEBUG_SERVER_DLL_SHA256
     );
 }
 
