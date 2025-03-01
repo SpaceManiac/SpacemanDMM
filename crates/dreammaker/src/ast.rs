5b4589e6ba28e5972828ac3b25d426b83857c524
harryob <55142896+harryob@users.noreply.github.com>
2025-03-01 08:55:05 +0000

    this too

--- crates/dreammaker/src/ast.rs
+++ crates/dreammaker/src/ast.rs
@@ -1077,6 +1077,10 @@ pub enum Term {
         library_name: Box<Expression>,
         function_name: Box<Expression>
     },
+    AsType {
+        value: Box<Expression>,
+        wanted_type: Box<Expression>
+    },
     /// Unscoped `::A` is a shorthand for `global.A`
     GlobalIdent(Ident2),
     /// Unscoped `::A(...)` is a shorthand for `global.A(...)`
