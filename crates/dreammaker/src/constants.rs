f61969093d02c2bee2b326fb9af93f7e0363d645
ZeWaka <zewakagamer@gmail.com>
2026-09-08 01:29:11 -0700

    const resolve fexists relative to included files
    `fexists()` needs the actual opened path to resolve relative arguments (usually environment-relative paths are used in the preprocessor/const-eval for lineinfo)
    
    this popped up because strongdmm doesn't set its cwd, and just uses whatever was passed by the shell, so it'd only resolve properly if cwd was set to your repo

--- crates/dreammaker/src/constants.rs
+++ crates/dreammaker/src/constants.rs
@@ -468,6 +468,7 @@ impl Expression {
             location,
             ty: NodeIndex::new(0),
             defines: None,
+            current_file_path: None,
         }
         .expr(self, None)
     }
@@ -479,9 +480,11 @@ pub fn preprocessor_evaluate(
     expr: &Expression,
     defines: &DefineMap,
     context: Option<&Context>,
+    current_file_path: &Path,
 ) -> Result<Constant, DMError> {
     ConstantFolder {
         context,
+        current_file_path: Some(current_file_path),
         tree: None,
         location,
         ty: NodeIndex::new(0),
@@ -577,6 +580,7 @@ fn constant_ident_lookup(
         context,
         tree: Some(tree),
         defines: None,
+        current_file_path: None,
         location,
         ty,
     }
@@ -597,6 +601,7 @@ fn constant_ident_lookup(
 
 struct ConstantFolder<'a> {
     context: Option<&'a Context>,
+    current_file_path: Option<&'a Path>,
     tree: Option<&'a mut ObjectTree>,
     defines: Option<&'a DefineMap>,
     location: Location,
@@ -856,7 +861,8 @@ impl<'a> ConstantFolder<'a> {
                 },
                 "fexists"
                     if self.defines.is_some()
-                        && let Some(context) = self.context =>
+                        && self.context.is_some()
+                        && let Some(current_file_path) = self.current_file_path =>
                 {
                     let [arg] = &args[..] else {
                         return Err(self.error(format!(
@@ -869,7 +875,6 @@ impl<'a> ConstantFolder<'a> {
                             self.error("malformed fexists() call, argument given isn't a string")
                         );
                     };
-                    let current_file_path = context.file_path(self.location.file);
                     let Some(current_dir) = current_file_path.parent() else {
                         return Err(self.error(format!(
                             "fexists() file has no parent: {current_file_path:?}"
--- crates/dreammaker/src/preprocessor.rs
+++ crates/dreammaker/src/preprocessor.rs
@@ -644,6 +644,7 @@ impl<'ctx> Preprocessor<'ctx> {
             &expr,
             &self.defines,
             Some(self.context),
+            self.include_stack.top_file_path(),
         )?
         .to_bool())
     }
