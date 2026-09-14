e1733ae490c9d4f931e2a48e8250ba5c7f82cd45
Tad Hardesty <tad@platymuus.com>
2026-09-13 20:24:18 -0700

    Add GitHub Actions build workflow

--- crates/dreammaker/tests/builtins_tests.rs
+++ crates/dreammaker/tests/builtins_tests.rs
@@ -1,8 +1,18 @@
 extern crate dreammaker as dm;
 
-use dm::objtree::ObjectTree;
+use dm::{objtree::ObjectTree, preprocessor::DefineMap};
 
 #[test]
-fn check_builtins() {
-    println!("{:?}", ObjectTree::with_builtins());
+fn check_builtin_types() {
+    let ot = ObjectTree::with_builtins();
+    assert!(ot.iter_types().count() >= 28);
+    for each in ot.iter_types() {
+        eprintln!("{:?}", each);
+    }
+}
+
+#[test]
+fn check_builtin_macros() {
+    let ot = DefineMap::with_builtins();
+    assert!(ot.len() >= 100);
 }
